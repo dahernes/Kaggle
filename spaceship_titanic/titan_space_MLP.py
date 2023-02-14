c50ccc4fb1fa3bf726acbfef77389a18380027b1
dahernes <dahernes86@googlemail.com>
2023-02-14 20:58:43 +0100

    MLP preprocessing and embeddet features

--- spaceship_titanic/titan_space_MLP.py
+++ spaceship_titanic/titan_space_MLP.py
@@ -33,9 +33,7 @@ titan_dp = pd.read_csv(data_folder / 'titan_df_cleaned.csv')
 print(titan_dp.dtypes)
 
 # %%
-# TODO Preprocessing
-
-# %%
+# Preprocessing
 # Normalization for numerical columns
 float_cols = titan_dp.select_dtypes(include='float64').columns
 int_cols = titan_dp.select_dtypes(include='int64').columns
@@ -68,7 +66,7 @@ titan_dp = titan_dp.drop(categorical_cols, axis=1)
 # Train / Valid Split
 titan = titan_dp.copy()
 X = titan
-y = X.pop('Transported').values
+y = X.pop('Transported_T').values
 
 print('Labels: {}'.format(set(y)))
 print('Zero count = {}, One count = {}'.format(len(y) - sum(y), sum(y)))
@@ -76,11 +74,47 @@ print('Zero count = {}, One count = {}'.format(len(y) - sum(y), sum(y)))
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=VALID_SIZE, random_state=RAND_SEED)
 
 # %%
-# TODO embedding layer for pytorch dataset
-# TODO create pytorch dataset
+# embedding layer for pytorch dataset
+# categorical embedding for columns having more than two values
+# embedded_cols = {n: len(col.cat.categories) for n,col in X.items() if len(col.cat.categories) > 2} größer 2 features
+# embedded_cols
+emb_object_features = ['HomePlanet_T', 'Destination_T', 'Cabin_Deck_T', 'Cabin_Side_T']
+embedded_cols = {}
+for col_i in emb_object_features:
+    embedded_cols[str(col_i)] = len(X[str(col_i)].unique())
+
+# %%
+# embedding sizes
+embedding_sizes = []
+for _, n_categories in embedded_cols.items():
+    size = (n_categories, min(50, (n_categories + 1) // 2))
+    embedding_sizes.append(size)
+
+
+# %%
+# create pytorch dataset
+class SpaceTitanicDataset(dataset):
+    def __int__(self, X, y, emb_object_features):
+        X = X.copy()
+        self.X1 = X.loc[:, emb_object_features].copy()
+        self.X2 = X.drop(columnms=emb_object_features.copy())
+        self.y = y
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, item):
+        return self.X1[item], self.X2[item], self.y[item]
+
+
+# creating train / valid datasets
+train_ds = SpaceTitanicDataset(X_train, y_train, emb_object_features)
+valid_ds = SpaceTitanicDataset(X_valid, y_valid, emb_object_features)
 
 # %% [markdown]
 # ## TODO MLP Model
+# TODO device compatible
+# TODO Model
 
 # %%
 # Train/Test Data Split
